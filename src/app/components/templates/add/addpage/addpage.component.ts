4c83b044374c9275403ee6db81243a4540eedf41
VictorHachard <nicolashachard@gmail.com>
2021-06-02 18:11:16 +0200

    Debug addpage

--- src/app/components/templates/add/addpage/addpage.component.ts
+++ src/app/components/templates/add/addpage/addpage.component.ts
@@ -17,6 +17,7 @@ export class AddpageComponent extends AbstractComponents implements OnInit {
 
   addPageForm: FormGroup;
   categories: Category[];
+  categoriesChild: Category[];
 
   constructor(private userSecurityService: UserSecurityService,
               private cookieService: CookieService,
@@ -30,14 +31,24 @@ export class AddpageComponent extends AbstractComponents implements OnInit {
   ngOnInit(): void {
     this.categoryService.getAllCategory().subscribe(value => {
       this.categories = value;
+      this.categoriesChild = [];
+      for (const cat of this.categories) {
+        if (cat.childCategory.length !== 0) {
+          for (const ch of cat.childCategory) {
+            this.categoriesChild.push(ch);
+          }
+        }
+      }
+      this.categories = this.categories.filter(x => x.childCategory.length === 0);
       console.log(this.categories);
+      console.log(this.categoriesChild);
       this.init();
     });
   }
 
   init(): void {
     this.addPageForm = new FormGroup({
-      category: new FormControl('none', Validators.required),
+      category: new FormControl(this.categoriesChild.length !== 0 ? this.categoriesChild[0].name : this.categories[0].name, Validators.required),
       title: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', [Validators.required, Validators.minLength(8), Validators.maxLength(128)]),
       description: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', [Validators.required, Validators.minLength(64), Validators.maxLength(1024)]),
     });
@@ -52,6 +63,14 @@ export class AddpageComponent extends AbstractComponents implements OnInit {
         categoryId = value.id;
       }
     });
+    this.categoriesChild.forEach(value => {
+      console.log(addPageValue.category + ' ' + value.name);
+      if (addPageValue.category === value.name) {
+        categoryId = value.id;
+      }
+    });
+
+    console.log(categoryId);
 
     this.pageService.addPage({categoryId: categoryId.toString(),
       description: addPageValue.description,
