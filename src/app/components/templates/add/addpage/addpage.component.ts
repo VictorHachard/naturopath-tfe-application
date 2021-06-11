ed5d5a74094c7617730ed223dd26710da2f21f39
VictorHachard <nicolashachard@gmail.com>
2021-06-11 13:32:51 +0200

    Debug category

--- src/app/components/templates/add/addpage/addpage.component.ts
+++ src/app/components/templates/add/addpage/addpage.component.ts
@@ -17,7 +17,6 @@ export class AddpageComponent extends AbstractComponents implements OnInit {
 
   addPageForm: FormGroup;
   categories: Category[];
-  categoriesChild: Category[];
 
   constructor(private userSecurityService: UserSecurityService,
               private cookieService: CookieService,
@@ -29,26 +28,16 @@ export class AddpageComponent extends AbstractComponents implements OnInit {
   }
 
   ngOnInit(): void {
-    this.categoryService.getAllCategory().subscribe(value => {
+    this.categoryService.getAllChildCategoryDto().subscribe(value => {
       this.categories = value;
-      this.categoriesChild = [];
-      for (const cat of this.categories) {
-        if (cat.childCategory.length !== 0) {
-          for (const ch of cat.childCategory) {
-            this.categoriesChild.push(ch);
-          }
-        }
-      }
-      this.categories = this.categories.filter(x => x.childCategory.length === 0);
       console.log(this.categories);
-      console.log(this.categoriesChild);
       this.init();
     });
   }
 
   init(): void {
     this.addPageForm = new FormGroup({
-      category: new FormControl(this.categoriesChild.length !== 0 ? this.categoriesChild[0].name : this.categories[0].name, Validators.required),
+      category: new FormControl(this.categories[0].name, Validators.required),
       title: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', [Validators.required, Validators.minLength(8), Validators.maxLength(128)]),
       description: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', [Validators.required, Validators.minLength(64), Validators.maxLength(1024)]),
     });
@@ -59,11 +48,6 @@ export class AddpageComponent extends AbstractComponents implements OnInit {
     let categoryId: number;
 
     this.categories.forEach(value => {
-      if (addPageValue.category === value.name) {
-        categoryId = value.id;
-      }
-    });
-    this.categoriesChild.forEach(value => {
       if (addPageValue.category === value.name) {
         categoryId = value.id;
       }
--- src/app/service/Category.service.ts
+++ src/app/service/Category.service.ts
@@ -31,6 +31,12 @@ export class CategoryService extends AbstractService {
       {headers : new HttpHeaders().set('Authorization', this.getUserJwt())});
   }
 
+  public getAllChildCategoryDto(): Observable<any> {
+    return this.http.get<any>(this.baseUrl + 'dto/getAllChild/',
+      {headers : new HttpHeaders().set('Authorization', this.getUserJwt())});
+  }
+
+
   public addCategory(body: any): Observable<any> {
     return this.http.post<any>(this.baseUrl, body,
       {headers : new HttpHeaders().set('Authorization', this.getUserJwt())});
